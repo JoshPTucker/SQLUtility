d3b5758d391047d242f2dd26e56adc443b72364a
Josh T <jptucker2009@gmail.com>
2016-06-30 11:51:34 -0400

    Functioning commit

---
+++ SQLUtilityTesting/SQLUtilityTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import SQLUtil.SQLUtility;
+
+public class SQLUtilityTest {
+
+	@Test
+	public void test() {
+		SQLUtility.executeSQL();
+	}
+
+}
